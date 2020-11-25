1f59a470d548eba9d10c0c64a96976572b28b01c
Martin Chalela <tinchochalela@gmail.com>
2020-11-25 18:14:18 -0300

    sounds manifest

--- snakejazz/__init__.py
+++ snakejazz/__init__.py
@@ -10,4 +10,6 @@ __version__ = "0.0.1"
 
 from . import sounds
 
-from .snakejazz import *  # noqa
+from .snakejazz import DEFAULT_START, DEFAULT_FINISH, DEFAULT_ERROR
+from .snakejazz import play_sound, decorator, zzz
+from .snakejazz import _parse_param
--- snakejazz/sounds/__init__.py
+++ snakejazz/sounds/__init__.py
@@ -12,12 +12,23 @@ import pathlib
 PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 
 AVAILABLE_PACKS = ["rhodesmas"]
-PACK_PATHS = [
-    PATH / name for name in AVAILABLE_PACKS if os.path.isdir(PATH / name)
-]
 
+
+for name in AVAILABLE_PACKS:
+    print(name)
+    if not os.path.isdir(PATH / name):
+        print(PATH / name)
+
+PACK_PATHS = [
+    str(PATH / name) for name in AVAILABLE_PACKS if os.path.isdir(PATH / name)
+]
+print(PACK_PATHS)
 RHODESMAS = {}
 for pack in PACK_PATHS:
+    print(pack)
     for name in os.listdir(pack):
+        print(name)
         if name.endswith(".wav"):
             RHODESMAS[name] = str(PATH / pack / name)
+
+print(RHODESMAS)
