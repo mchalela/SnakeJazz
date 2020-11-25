a90f8bf6def60fcbb7bdf37a74e4e3a22da288a4
Martin Chalela <tinchochalela@gmail.com>
2020-11-25 18:41:04 -0300

    manifest change

--- snakejazz/sounds/rhodesmas/__init__.py
+++ snakejazz/sounds/rhodesmas/__init__.py
@@ -14,6 +14,8 @@ PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 PACK_NAME = "rhodesmas"
 
 RHODESMAS = {}
+print(os.listdir(PATH))
 for name in os.listdir(PATH):
+    print(name)
     if name.endswith(".wav"):
         RHODESMAS[name] = str(PATH / name)
